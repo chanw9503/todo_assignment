cabef582ba6dc72e6bdf95e15311bf50482ac5cc
ChanwooPark <chanw9503@gmail.com>
2023-02-27 16:47:11 +0900

    style.css setting

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
 import React from 'react'
-
+import './App.css'
 function App() {
   return (
-    <div>App</div>
+    <div>
+      <div className='wrap'>
+        <div className='container'>
+          <div className='wrap_header'>
+            <div className="header_text">
+              <div className='dateBox'>
+                날짜/요일
+              </div>
+              <div className='todoListCount'>
+                todoList 갯수
+              </div>
+            </div>
+            <div className='add_Btn_Box'>
+              <button className='addBtn'>+</button>
+            </div>
+          </div>
+          <div className='toDoCard'>
+
+          </div>
+
+        </div>
+
+      </div>
+    </div>
   )
 }
 
